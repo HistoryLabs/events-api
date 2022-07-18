490ef109f3bdb90a1d172daed390a801eeee59c4
Angoose <angoose.dev@gmail.com>
2022-07-17 22:12:12 -0500

    Clear repo

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.use('/', (req, res) => {
-    res.send('This is the events-api! Welecome!');
-});
-
-app.listen(3000, () => console.log('events-api listening on port 3000'));
