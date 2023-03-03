bb21723873c7a8f22f63a623e47fd34df01965e6
Angoose <angoose.dev@gmail.com>
2023-03-02 21:24:27 -0600

    Add backwards compatibility tests for GET /year
    Tests ensure the path parameter variant and the query parameter variant both work.

--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"github.com/HistoryLabs/events-api/data"
 	"github.com/HistoryLabs/events-api/server/routes"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"net/http/httptest"
+	"reflect"
 	"strconv"
 	"testing"
 )
@@ -57,30 +59,63 @@ func TestDate(t *testing.T) {
 
 func TestYear(t *testing.T) {
 	router := initRouter()
-	router.GET("/year", routes.FetchYear)
+	router.GET("/year/*year", routes.FetchYear)
 
-	req := httptest.NewRequest("GET", "/year?year=1776&onlyDated=true", nil)
+	testEvents := func(yearData data.YearDto) {
+		if yearData.TotalResults <= 0 {
+			t.Errorf("Expected at least one Event but got 0")
+			return
+		}
+
+		for _, event := range yearData.Events {
+			if event.Date == "" {
+				prettyEvent, _ := json.MarshalIndent(event, "", "\t")
+				t.Errorf("Expected all Events to have non-empty Date, but found Event without Date:\n%s", prettyEvent)
+			}
+		}
+	}
+
+	getBody := func(rawBody *bytes.Buffer) data.YearDto {
+		body, _ := ioutil.ReadAll(rawBody)
+		var yearData data.YearDto
+		json.Unmarshal(body, &yearData)
+		return yearData
+	}
+
+	// Test path parameter version of endpoint
+
+	req := httptest.NewRequest("GET", "/year/1776?onlyDated=true", nil)
 	rr := httptest.NewRecorder()
 	router.ServeHTTP(rr, req)
 
 	if rr.Result().StatusCode != 200 {
-		t.Errorf("Expected status code 200 but got: %s", rr.Result().Status)
+		t.Errorf("[Path Param] Expected status code 200 but got: %s", rr.Result().Status)
 		return
 	}
 
-	body, _ := ioutil.ReadAll(rr.Body)
-	var yearData data.YearDto
-	json.Unmarshal(body, &yearData)
+	pathData := getBody(rr.Body)
 
-	if yearData.TotalResults <= 0 {
-		t.Errorf("Expected at least one Event but got 0")
+	testEvents(pathData)
+
+	// Test query parameter version of endpoint
+
+	req = httptest.NewRequest("GET", "/year/?year=1776&onlyDated=true", nil)
+	rr = httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if rr.Result().StatusCode != 200 {
+		t.Errorf("[Query Param] Expected status code 200 but got: %s", rr.Result().Status)
 		return
 	}
 
-	for _, event := range yearData.Events {
-		if event.Date == "" {
-			prettyEvent, _ := json.MarshalIndent(event, "", "\t")
-			t.Errorf("Expected all Events to have non-empty Date, but found Event without Date:\n%s", prettyEvent)
-		}
+	queryData := getBody(rr.Body)
+
+	testEvents(queryData)
+
+	// Ensure responses are the same from each endpoint version
+
+	if reflect.DeepEqual(pathData, queryData) == false {
+		t.Errorf("Expected path param and query param responses to be equal but got different responses")
+		return
 	}
 }
